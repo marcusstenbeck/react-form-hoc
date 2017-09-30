e89bd7d9a8cd05998d3d14e5b5b963e2e0cedcfa
marcusstenbeck <marcus.stenbeck@gmail.com>
2017-09-30 12:30:58 +0200

    use class and import prop types from new package

--- module/index.js
+++ module/index.js
@@ -1,137 +1,135 @@
-import React, { PropTypes as P } from 'react'
-import shallowequal from 'shallowequal'
+import React from "react";
+import P from "prop-types";
+import shallowequal from "shallowequal";
 
-const all = (xs, fn) => !!Object.keys(xs).reduce((acc, x) => {
-  return acc && fn(xs[x])
-}, true)
+const all = (xs, fn) =>
+  !!Object.keys(xs).reduce((acc, x) => {
+    return acc && fn(xs[x]);
+  }, true);
 const append = (key, value, obj) => ({
   ...obj,
   [key]: {
     ...obj[key],
     ...value
   }
-})
+});
 
-export default function reactFormHoc (options = {}) {
-  return (Component) => {
-    const {
-      fields = [],
-      validate = () => {}
-    } = options
+export default function reactFormHoc(options = {}) {
+  return Component => {
+    const { fields = [], validate = () => {} } = options;
 
-    return React.createClass({
-      displayName: 'react-form-hoc',
+    return class ReactFormHoc extends React.Component {
+      static displayName = "react-form-hoc";
 
-      propTypes: {
+      static propTypes = {
         onChange: P.func,
         initialValues: P.object,
         validate: P.bool
-      },
+      };
 
-      getDefaultProps () {
-        return {
-          onChange: () => {},
-          initialValues: {},
-          validate: false,
-          touchAll: false
-        }
-      },
+      static defaultProps = {
+        onChange: () => {},
+        initialValues: {},
+        validate: false,
+        touchAll: false
+      };
 
-      getInitialState () {
-        return this.stateFromProps(this.props)
-      },
+      constructor(props, ...rest) {
+        super(props, ...rest);
 
-      stateFromProps ({ initialValues, touchAll }) {
-        return fields.reduce((acc, name) => ({
-          ...acc,
-          values: initialValues,
-          fields: {
-            ...acc.fields,
-            [name]: {
-              name,
-              error: validate(name, initialValues[name]),
-              touched: touchAll,
-              value: initialValues[name],
-              initialValue: initialValues[name],
-              onChange: this.handleChanges(name),
-              onBlur: this.handleBlur(name)
+        this.state = this.stateFromProps(props);
+      }
+
+      stateFromProps = ({ initialValues, touchAll }) => {
+        return fields.reduce(
+          (acc, name) => ({
+            ...acc,
+            values: initialValues,
+            fields: {
+              ...acc.fields,
+              [name]: {
+                name,
+                error: validate(name, initialValues[name]),
+                touched: touchAll,
+                value: initialValues[name],
+                initialValue: initialValues[name],
+                onChange: this.handleChanges(name),
+                onBlur: this.handleBlur(name)
+              }
             }
+          }),
+          {
+            fields: {}
           }
-        }), {
-          fields: {}
-        })
-      },
+        );
+      };
 
-      componentWillReceiveProps (next) {
-        const { initialValues, touchAll } = this.props
+      componentWillReceiveProps(next) {
+        const { initialValues, touchAll } = this.props;
         if (!shallowequal(next.initialValues, initialValues)) {
-          this.setState(this.stateFromProps(next))
+          this.setState(this.stateFromProps(next));
         }
         if (next.touchAll !== touchAll) {
-          this.touchAll()
+          this.touchAll();
         }
-      },
+      }
 
-      touchAll () {
+      touchAll = () => {
         this.setState({
           ...this.state,
-          fields: fields.reduce((acc, name) => ({
-            ...acc,
-            [name]: {
-              ...acc[name],
-              touched: true
-            }
-          }), this.state.fields)
-        })
-      },
+          fields: fields.reduce(
+            (acc, name) => ({
+              ...acc,
+              [name]: {
+                ...acc[name],
+                touched: true
+              }
+            }),
+            this.state.fields
+          )
+        });
+      };
 
-      allValid (state) {
-        const { fields } = state
-        return all(fields, (field) => !field.error)
-      },
+      allValid = state => {
+        const { fields } = state;
+        return all(fields, field => !field.error);
+      };
 
-      handleChanges (key) {
-        return (e) => {
-          const { fields, values } = this.state
-          const { value } = e.target
-          const error = validate(key, value)
+      handleChanges = key => {
+        return e => {
+          const { fields, values } = this.state;
+          const { value } = e.target;
+          const error = validate(key, value);
           const newState = {
             ...this.state,
             values: { ...values, [key]: value },
             fields: append(key, { value, error }, fields)
-          }
-          this.setState(newState)
-          this.props.onChange(
-              newState.values,
-              this.allValid(newState)
-          )
-        }
-      },
+          };
+          this.setState(newState);
+          this.props.onChange(newState.values, this.allValid(newState));
+        };
+      };
 
-      handleBlur (key) {
+      handleBlur = key => {
         return () => {
-          const { fields } = this.state
-          const field = fields[key]
-          const error = validate(key, field.value)
+          const { fields } = this.state;
+          const field = fields[key];
+          const error = validate(key, field.value);
           this.setState({
             ...this.state,
             fields: {
               ...fields,
               [key]: { ...field, touched: true, error }
             }
-          })
-        }
-      },
+          });
+        };
+      };
 
-      render () {
-        const { state, props } = this
+      render() {
+        const { state, props } = this;
 
-        return (
-          <Component
-            { ...props }
-            { ...state }
-          />)
+        return <Component {...props} {...state} />;
       }
-    })
-  }
+    };
+  };
 }
